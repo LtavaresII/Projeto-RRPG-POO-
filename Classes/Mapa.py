5743ac9a5cf517a2aedc9fed394042cc26ef63fa
LtavaresII <30603702+LtavaresII@users.noreply.github.com>
2019-11-18 22:49:58 -0300

    Add files via upload

---
+++ Classes/Mapa.py
@@ -0,0 +1,26 @@
+#!-*- conding: utf8 -*
+import sys
+import json
+from tinydb import TinyDB, Query
+import random
+import pickle
+import socket
+import select 
+from Erros import ErroComandoInvalido,ErroOpcaoNaoValida,ErroInteracao
+
+class Mapas:
+	def __init__(self):
+		
+	def Upload(self):
+		
+	def Personificar(self):
+		
+	def Esticar(self):
+	
+	def Mover(self):
+	
+
+
+
+if __name__ == '__main__':
+
